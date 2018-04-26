6c5a54efbf6650acccacbda2b7fe153f9ee278d2
MaoScut <849345372@qq.com>
2018-04-26 22:37:44 +0800

    帮涛哥画的有向图，用到了d3 js

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ROOT_PATH = path.resolve(__dirname);
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js',
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+  },
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      loader: ['babel-loader'],
+    },
+    {
+      test: /\.scss$/,
+      loader: ['style-loader', 'css-loader', 'sass-loader'],
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+  ],
+};
